f5f1c20119ab84509de9ec3fd32cdfbbec903239
GustavoG <126207720+ggSOS@users.noreply.github.com>
2025-08-18 09:03:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Ambiente1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
