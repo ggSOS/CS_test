b80092c554e6fbbb045de742047d1071b0278432
GustavoG <126207720+ggSOS@users.noreply.github.com>
2025-08-18 09:46:08 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,28 @@ namespace Ambiente1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Dois números serão somados, e resultado será comparado a zero.");
+
+            Console.Write("Digite o primeiro número a ser somado:\n\t- ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número a ser somado:\n\t- ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int n3 = n1 + n2;
+            String resultado;
+            if (n3 < 0)
+            {
+                resultado = "menor";
+            }else if (n3 == 0)
+            {
+                resultado = "igual";
+            }
+            else
+            {
+                resultado = "maior";
+            }
+            Console.Write("A soma ({0}) é {1} que zero", n3, resultado);
         }
     }
 }
